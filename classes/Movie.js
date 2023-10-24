503de84b658be8dd64014d31487e2a093e48a027
Yunusade <yunus.adebayo@sky.uk>
2023-10-24 15:53:58 +0100

    First Commit

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,22 @@
 // import the Media class:
-
+const Media = require('./Media')
 // create your Movie class:
+class Movie extends Media {
+  constructor(title, year, genre, director, duration, rating){
+    super(title, year, genre)
+    this.director = director;
+    this.duration = duration;
+    this.rating = rating
+  }
+
+  summary() {
+    return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`
+  }
+
+  static longestMovie(movie) {
+    return movie.sort((a, b) => b.duration - a.duration)[0]
+  }
+}
 
 // don't change below
 module.exports = Movie;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,20 @@
 // import the Media class:
-
+const Media = require('./Media')
 // create your Music class:
+class Music extends Media {
+  constructor(title, year, genre, artist, length){
+    super(title, year, genre)
+    this.artist = artist;
+    this.length = length
+  }
 
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+  }
+
+  static shortestAlbum(music) {
+    return music.sort((a, b) => a.length - b.length)[0]
+  }
+}
 // don't change below
 module.exports = Music;
